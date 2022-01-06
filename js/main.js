234e9b4badd2ddaac39548bd3c6bae67435e5881
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-01-06 14:31:40 +0100

    Grasshopper - Bug Squashing

--- js/main.js
+++ js/main.js
@@ -536,3 +536,19 @@
 // 	var total = num + 5;
 // 	return total;
 // }
+//
+//
+// Grasshopper - Bug Squashing
+
+// var health = 100;
+// var position = 0;
+// var coins = 0;
+
+// function main() {
+// 	rollDice();
+// 	move();
+// 	combat();
+// 	getCoins();
+// 	buyHealth();
+// 	printStatus();
+// }
