9423ae640202f0a4f13df280dd180281318730d2
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-01-23 16:46:12 +0100

    Find numbers which are divisible by given number

--- js/main.js
+++ js/main.js
@@ -267,3 +267,16 @@
 // 	}
 // 	return sum;
 // }
+//
+//
+// Find numbers which are divisible by given number
+
+// function divisibleBy(numbers, divisor) {
+// 	let arr = [];
+// 	for (let i = 0; i < numbers.length; i++) {
+// 		if (numbers[i] % divisor === 0) {
+// 			arr.push(numbers[i]);
+// 		}
+// 	}
+// 	return arr;
+// }
