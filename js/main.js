8b03c33cbbd133f3920723dc91c5894b43244ae2
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-03-13 11:33:55 +0100

    Switch it Up!

--- js/main.js
+++ js/main.js
@@ -394,3 +394,42 @@
 // Will there be enough space?
 // const enough = (cap, on, wait) =>
 // 	cap - on > wait ? 0 : Math.abs(cap - on - wait);
+//
+// Switch it Up!
+// function switchItUp(number) {
+// 	switch (number) {
+// 		case 1:
+// 			return "One";
+// 			break;
+// 		case 2:
+// 			return "Two";
+// 			break;
+// 		case 3:
+// 			return "Three";
+// 			break;
+// 		case 4:
+// 			return "Four";
+// 			break;
+// 		case 5:
+// 			return "Five";
+// 			break;
+// 		case 6:
+// 			return "Six";
+// 			break;
+// 		case 7:
+// 			return "Seven";
+// 			break;
+// 		case 8:
+// 			return "Eight";
+// 			break;
+// 		case 9:
+// 			return "Nine";
+// 			break;
+// 		case 0:
+// 			return "Zero";
+// 			break;
+// 		default:
+// 			return "Try again";
+// 			break;
+// 	}
+// }
