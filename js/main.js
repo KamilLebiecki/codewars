4ee5683f9ff66b79e40601dd5ea21155b3210dd0
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-03-26 12:34:17 +0100

    Is this a triangle?

--- js/main.js
+++ js/main.js
@@ -460,3 +460,7 @@
 // function createPhoneNumber(numbers) {
 // 	return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
 // }
+
+// Is this a triangle?
+// const isTriangle = (a, b, c) =>
+// 	a < b + c && b < a + c && c < a + b ? true : false;
