537ac21dcb86a13c81bddf3d789f4a50af07650b
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-05 23:09:00 +0200

    String repeat

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 // Reversed Strings
-const solution = (str) => str.split("").reverse().join("");
+// const solution = (str) => str.split("").reverse().join("");
+
+// String repeat
+// const repeatStr = (n, s) => (n > 0 ? s.repeat(n) : "");
