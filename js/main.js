3c257f49557f6f40b45c370a4e4cde5a0e8b3ca0
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-06 07:44:45 +0200

    Square(n) Sum

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@
 
 // Remove String Spaces
 // const noSpace = (x) => x.split(" ").join("");
+
+// Square(n) Sum
+
+// function squareSum(numbers) {
+// 	let result = 0;
+// 	for (let i = 0; i < numbers.length; i++) {
+// 		result += numbers[i] ** 2;
+// 	}
+// 	return result;
+// }
