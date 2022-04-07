7835876e93769595f5a823c939d471cc3096eb39
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-07 16:57:36 +0200

    Sum Arrays

--- js/main.js
+++ js/main.js
@@ -108,3 +108,15 @@
 // 	}
 // 	return sheepCounter;
 // }
+
+// Sum Arrays
+
+// function sum(numbers) {
+// 	let sum = 0;
+// 	if (numbers.length !== 0) {
+// 		for (let i = 0; i < numbers.length; i++) {
+// 			sum += numbers[i];
+// 		}
+// 	}
+// 	return sum;
+// }
