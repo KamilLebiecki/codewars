66798564f529940effa46af39b4bdf9a804d6d0f
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-11 16:56:18 +0200

    Grasshopper - Terminal Game #1

--- js/main.js
+++ js/main.js
@@ -648,3 +648,16 @@
 //
 // Grasshopper - Check for factor
 // const checkForFactor = (base, factor) => base % factor === 0
+//
+// Grasshopper - Terminal Game #1
+// function Hero(name) {
+// 	if (!name) {
+// 		this.name = "Hero";
+// 	} else {
+// 		this.name = name;
+// 	}
+// 	this.position = "00";
+// 	this.health = 100;
+// 	this.damage = 5;
+// 	this.experience = 0;
+// }
