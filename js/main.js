a0f83fcefce757684e6cbb3101ac316bbcc352fa
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-12 12:33:55 +0200

    BASIC: Making Six Toast.

--- js/main.js
+++ js/main.js
@@ -737,12 +737,15 @@
 // }
 
 // Shifty Closures
-var greet_abe = function () {
-	var name = "Abe";
-	return "Hello, " + name + "!";
-};
+// var greet_abe = function () {
+// 	var name = "Abe";
+// 	return "Hello, " + name + "!";
+// };
 
-var greet_ben = function () {
-	var name = "Ben";
-	return "Hello, " + name + "!";
-};
+// var greet_ben = function () {
+// 	var name = "Ben";
+// 	return "Hello, " + name + "!";
+// };
+//
+// BASIC: Making Six Toast.
+// const sixToast = (num) => Math.abs(6 - num);
