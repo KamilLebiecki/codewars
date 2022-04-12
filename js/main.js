e8c3bb9c4e8f3db9ae486ebec78a254af3c03f79
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-12 11:12:13 +0200

    Student's Final Grade

--- js/main.js
+++ js/main.js
@@ -695,3 +695,14 @@
 //
 // L1: Set Alarm
 // const setAlarm = (employed, vacation) => employed && !vacation;
+//
+// Student's Final Grade
+// function finalGrade(exam, projects) {
+// 	return exam > 90 || projects > 10
+// 		? 100
+// 		: exam > 75 && projects >= 5
+// 		? 90
+// 		: exam > 50 && projects >= 2
+// 		? 75
+// 		: 0;
+// }
