b4e4201a557f228d9c18711056d360faf8360847
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-13 13:01:21 +0200

    Multiplication table for number

--- js/main.js
+++ js/main.js
@@ -764,10 +764,10 @@
 //
 //
 // Multiplication table for number
-function multiTable(number) {
-	let result = "";
-	for (let i = 1; i <= 10; i++) {
-		result += `${i} * ${number} = ${i * number}`;
-	}
-	return result;
-}
+// function multiTable(number) {
+// 	let result = "";
+// 	for (let i = 1; i <= 10; i++) {
+// 		result += `${i} * ${number} = ${i * number}\n`;
+// 	}
+// 	return result.slice(0, -1);
+// }
