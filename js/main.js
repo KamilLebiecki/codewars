9f50b555ff6d2fbaee33f3d41d75a9fe233967d2
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-21 08:00:00 +0200

    Generate range of integers

--- js/main.js
+++ js/main.js
@@ -1082,3 +1082,48 @@
 // 	}
 // 	return arr;
 // }
+//
+// Cat years, Dog years
+// var humanYearsCatYearsDogYears = function (humanYears) {
+// 	let result = Array(3);
+// 	let catYears = 0;
+// 	let dogYears = 0;
+
+// 	if (humanYears === 1) {
+// 		catYears = 15;
+// 	} else if (humanYears === 2) {
+// 		catYears = 24;
+// 	} else {
+// 		catYears = 15 + 24 + 4 * humanYears;
+// 	}
+
+// 	if (humanYears === 1) {
+// 		// catYears = 15;
+// 		dogYears = 15;
+// 	} else if (humanYears === 2) {
+// 		// catYears = 24;
+// 		dogYears = 24;
+// 	} else {
+// 		// catYears = 24 + 4 * humanYears
+// 		dogYears = 24 + 5 * humanYears;
+// 	}
+// 	return result.push(humanYears, catYears, dogYears);
+// };
+
+// Cat years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+//
+// Generate range of integers
+function generateRange(min, max, step) {
+	let array = [];
+	for (let i = min; i <= max; i += step) {
+		array.push(i);
+	}
+	return array;
+}
