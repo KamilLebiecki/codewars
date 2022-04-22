317b15e2d04fff18f6488b11a015f817436523e3
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-22 17:36:47 +0200

    Welcome

--- js/main.js
+++ js/main.js
@@ -1149,3 +1149,44 @@
 //
 // Add Length
 // const addLength = (str) => str.split(" ").map((e) => `${e} ${e.length}`);
+//
+// Welcome!
+// function greet(language) {
+// 	switch (language) {
+// 		case "czech":
+// 			return "Vitejte";
+// 		case "danish":
+// 			return "Velkomst";
+// 		case "dutch":
+// 			return "Welkom";
+// 		case "estonian":
+// 			return "Tere tulemast";
+// 		case "finnish":
+// 			return "Tervetuloa";
+// 		case "flemish":
+// 			return "Welgekomen";
+// 		case "french":
+// 			return "Bienvenue";
+// 		case "german":
+// 			return "Willkommen";
+// 		case "irish":
+// 			return "Failte";
+// 		case "italian":
+// 			return "Benvenuto";
+// 		case "latvian":
+// 			return "Gaidits";
+// 		case "lithuanian":
+// 			return "Laukiamas";
+// 		case "polish":
+// 			return "Witamy";
+// 		case "spanish":
+// 			return "Bienvenido";
+// 		case "swedish":
+// 			return "Valkommen";
+// 		case "welsh":
+// 			return "Croeso";
+// 		case "english":
+// 		default:
+// 			return "Welcome";
+// 	}
+// }
