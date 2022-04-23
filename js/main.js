e5e856aab9b8cad45bfd9faf8526fa7218fa4cf4
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-04-23 07:35:42 +0200

    A bugs trilogy: Episode 1 - Let Math.Random(); decide your future

--- js/main.js
+++ js/main.js
@@ -1233,3 +1233,18 @@
 // Push a hash/an object into array
 // items = [];
 // items.push({ a: "b", c: "d" });
+//
+// A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+//
+// function yourFutureCareer() {
+// 	let career = Math.random();
+// 	if (career <= 0.32) {
+// 		return "FrontEnd Developer";
+// 	} else if (career <= 0.65) {
+// 		return "BackEnd Developer";
+// 	} else {
+// 		return "Full-Stack Developer";
+// 	}
+// }
+
+// yourFutureCareer();
