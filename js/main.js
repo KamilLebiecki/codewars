62085aff272e3025f93c8343cea640c2ac7cfde5
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-10 11:26:31 +0200

    Training JS #11: loop statement --break, continue

--- js/main.js
+++ js/main.js
@@ -1461,3 +1461,43 @@
 // 	} while (i < n);
 // 	return str;
 // }
+//
+// Training JS #11: loop statement --break,continue
+// const grabDoll = (dolls) => {
+// 	let bag = [];
+// 	for (let i = 0; i < dolls.length; i++) {
+// 		if (bag.length === 3) {
+// 			break;
+// 		}
+// 		if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") continue;
+// 		bag.push(dolls[i]);
+// 	}
+// 	return bag;
+// };
+
+// const grabDoll = (dolls) => {
+// 	let bag = [];
+// 	for (let i = 0; i < dolls.length; i++) {
+// 		if (bag.length === 3) {
+// 			break;
+// 		}
+// 		if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") continue;
+// 		bag.push(dolls[i]);
+// 	}
+// 	return bag;
+// };
+
+// console.log(
+// 	grabDoll([
+// 		"Hello Kitty",
+// 		"Barbie doll",
+// 		"Woody",
+// 		"Alien",
+// 		"Hello Kitty",
+// 		"Barbie doll",
+// 		"Buzz Astral",
+// 		"Sid",
+// 		"Hello Kitty",
+// 		"Barbie doll",
+// 	])
+// );
