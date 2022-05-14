3a24fbbffa41cd4586cc0429401fe161b1b66667
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-14 15:52:59 +0200

    Training JS #12: loop statement --for..in and for..of

--- js/main.js
+++ js/main.js
@@ -1518,11 +1518,15 @@
 //
 // Training JS #12: loop statement --for..in and for..of
 
-function giveMeFive(obj) {
-	let array = [];
-	for (var key in obj) {
-		if (key.length === 5) {
-			array.push(key);
-		}
-	}
-}
+// function giveMeFive(obj) {
+// 	let array = [];
+// 	for (var key in obj) {
+// 		if (key.length === 5) {
+// 			array.push(key);
+// 		}
+// 		if (obj[key].length === 5) {
+// 			array.push(obj[key]);
+// 		}
+// 	}
+// 	return array;
+// }
