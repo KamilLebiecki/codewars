c2eab72d4ec79f033ab8042f7f8b23a2d4f8be02
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-18 10:42:37 +0200

    Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode()

--- js/main.js
+++ js/main.js
@@ -1610,9 +1610,74 @@
 // 		.join(" ");
 //
 // Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode()
-
-// split the string into words
-// split the words into letters
-// decrypt letters with shift left 3
-// join the letters into words
-// join words into string
+// function topSecret(str) {
+// 	let dict = [
+// 		"a",
+// 		"b",
+// 		"c",
+// 		"d",
+// 		"e",
+// 		"f",
+// 		"g",
+// 		"h",
+// 		"i",
+// 		"j",
+// 		"k",
+// 		"l",
+// 		"m",
+// 		"n",
+// 		"o",
+// 		"p",
+// 		"q",
+// 		"r",
+// 		"s",
+// 		"t",
+// 		"u",
+// 		"v",
+// 		"w",
+// 		"x",
+// 		"y",
+// 		"z",
+// 	];
+// 	let Dict = [
+// 		"A",
+// 		"B",
+// 		"C",
+// 		"D",
+// 		"E",
+// 		"F",
+// 		"G",
+// 		"H",
+// 		"I",
+// 		"J",
+// 		"K",
+// 		"L",
+// 		"M",
+// 		"N",
+// 		"O",
+// 		"P",
+// 		"Q",
+// 		"R",
+// 		"S",
+// 		"T",
+// 		"U",
+// 		"V",
+// 		"W",
+// 		"X",
+// 		"Y",
+// 		"Z",
+// 	];
+// 	return str.replace(/[a-z]/gi, (v) => {
+// 		if (v === v.toLowerCase()) {
+// 			return dict.indexOf(v.toLowerCase()) - 3 < 0
+// 				? dict[dict.indexOf(v.toLowerCase()) - 3 + 26]
+// 				: dict[dict.indexOf(v.toLowerCase()) - 3];
+// 		}
+// 		if (v === v.toUpperCase()) {
+// 			return Dict.indexOf(v) - 3 < 0
+// 				? Dict[Dict.indexOf(v) - 3 + 26]
+// 				: Dict[Dict.indexOf(v) - 3];
+// 		}
+// 		return v;
+// 	});
+// }
