d83625bc725f88bac0c72dd7fe8c7fa3c18849e8
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-19 12:44:02 +0200

    Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+// reversedStrings
+//const solution = (str) => str.split("").reverse().join("");
+
 // String repeat
-//
+// const repeatStr = (n, s) => (n > 0 ? s.repeat(n) : "");
 
 // Remove String Spaces
 // const noSpace = (x) => x.split(" ").join("");
--- js/stringRepeat.js
+++
@@ -1,1 +0,0 @@
-const repeatStr = (n, s) => (n > 0 ? s.repeat(n) : "");
