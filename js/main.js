b66ee155381b348d8b31c68802737548b34f7332
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-30 16:53:30 +0200

    Training JS #24: methods of arrayObject---splice() and slice()

--- js/main.js
+++ js/main.js
@@ -1732,3 +1732,12 @@
 // }
 //
 // Training JS #24: methods of arrayObject---splice() and slice()
+
+// Coding in function threeInOne. function accept 1 parameters arr, it's a 1D number array. Your task is to merge each of the 3 elements into 1 elements (sum value) and return the result.
+// function threeInOne(arr) {
+// 	let newArr = [];
+// 	for (let i = 0; i < arr.length; i += 3) {
+// 		newArr.push(arr[i] + arr[i + 1] + arr[i + 2]);
+// 	}
+// 	return newArr;
+// }
