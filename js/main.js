3712818c9ab2efa34e3632e8870d7a9e105ffc03
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-30 12:47:46 +0200

    Training JS #23: methods of arrayObject---push(), pop(), shift() and unshift()

--- js/main.js
+++ js/main.js
@@ -1716,21 +1716,20 @@
 
 // Finally, return arr.
 
-function infiniteLoop(arr, d, n) {
-	for (let i = 1; i <= n; i++) {
-		if (d === "left") {
-			arr[2].push(arr[0].shift());
-			arr[1].push(arr[2].shift());
-			arr[0].push(arr[1].shift());
-		}
-		if (d === "right") {
-			arr[0].unshift(arr[2].pop());
-			arr[1].unshift(arr[0].pop());
-			arr[2].unshift(arr[1].pop());
-		}
-	}
-	return arr;
-}
+// function infiniteLoop(arr, d, n) {
+// 	for (let i = 0; i < n; i++) {
+// 		if (d === "right") {
+// 			arr[0].unshift(arr[2].pop());
+// 			arr[1].unshift(arr[0].pop());
+// 			arr[2].unshift(arr[1].pop());
+// 		} else if (d === "left") {
+// 			arr[2].push(arr[0].shift());
+// 			arr[1].push(arr[2].shift());
+// 			arr[0].push(arr[1].shift());
+// 		}
+// 	}
+// 	return arr;
+// }
 
 // infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"left",1) //should return [[2,3,4],[5,6,7],[8,9,1]]
 // infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"right",1) //should return [[9,1,2],[3,4,5],[6,7,8]]
