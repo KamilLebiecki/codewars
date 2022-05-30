27530f2d9b7956dfee87a31d8df1138a6de7d5ea
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-05-30 21:08:12 +0200

    Training JS #23

--- js/main.js
+++ js/main.js
@@ -1718,6 +1718,23 @@
 
 function infiniteLoop(arr, d, n) {
 	let newArr = [];
+	//
+}
+
+function infiniteLoop(arr, d, n) {
+	for (let i = 1; i <= n; i++) {
+		if (d === "left") {
+			arr[2].push(arr[0].shift());
+			arr[1].push(arr[2].shift());
+			arr[0].push(arr[1].shift());
+		}
+		if (d === "right") {
+			arr[0].unshift(arr[2].pop());
+			arr[1].unshift(arr[0].pop());
+			arr[2].unshift(arr[1].pop());
+		}
+	}
+	return arr;
 }
 
 // infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"left",1) //should return [[2,3,4],[5,6,7],[8,9,1]]
