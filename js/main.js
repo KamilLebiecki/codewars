a2d113efee6b63ce094b1ba05ce3120d1883d6d2
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-06-29 07:25:12 +0200

    codewars repetition

--- js/main.js
+++ js/main.js
@@ -2182,10 +2182,14 @@
 // 			return `I smell a series!`;
 // 	}
 // }
-function well(x) {
-	var good = 0;
-	for (var i = 0; i < x.length; i++)
-		if (x[i] == "good" && ++good > 2) return "I smell a series!";
+// function well(x) {
+// 	var good = 0;
+// 	for (var i = 0; i < x.length; i++)
+// 		if (x[i] == "good" && ++good > 2) return "I smell a series!";
 
-	return good ? "Publish!" : "Fail!";
-}
+// 	return good ? "Publish!" : "Fail!";
+// }
+// function well(x) {
+// 	const count = x.reduce((s, v) => s + (v == "good"), 0);
+// 	return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
+// }
