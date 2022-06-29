6a8b57647d14a779f3414b9899b6e66d2708d814
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-06-29 07:20:00 +0200

    codewars repetition

--- js/main.js
+++ js/main.js
@@ -2156,3 +2156,17 @@
 // function correct(string) {
 // 	return [...string].map((a) => ({ 0: "O", 5: "S", 1: "I" }[a] || a)).join("");
 // }
+
+// const well = (x) => {
+// 	let result = 0;
+// 	for (let i = 0; i < x.length; i++) {
+// 		if (x[i] === "good") {
+// 			result++;
+// 		}
+// 		return result === 0
+// 			? "Fail!"
+// 			: result > 2
+// 			? "I smell a series!"
+// 			: "Publish!";
+// 	}
+// };
