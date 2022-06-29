f883692f84eba1e04fd02b0a4981ce2268ce1982
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-06-29 07:18:02 +0200

    codewars repetition

--- js/main.js
+++ js/main.js
@@ -2152,3 +2152,7 @@
 // 		.map((c) => (map.hasOwnProperty(c) ? map[c] : c))
 // 		.join("");
 // }
+
+// function correct(string) {
+// 	return [...string].map((a) => ({ 0: "O", 5: "S", 1: "I" }[a] || a)).join("");
+// }
