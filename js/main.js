add6934d561a7c43e584d8be1be34d3909329041
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-06-30 06:34:05 +0200

    codewars repetition

--- js/main.js
+++ js/main.js
@@ -2239,3 +2239,15 @@
 // 		? "Publish!"
 // 		: "I smell a series!";
 // };
+
+function well(x) {
+	switch (x.filter((i) => i === "good").length) {
+		case 0:
+			return "Fail!";
+		case 1:
+		case 2:
+			return "Publish!";
+		default:
+			return "I smell a series!";
+	}
+}
