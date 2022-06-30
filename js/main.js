267679eed642e5ed3c2dab98af7347e1b5fef75e
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-06-30 06:46:23 +0200

    codewars repetition

--- js/main.js
+++ js/main.js
@@ -2277,3 +2277,15 @@
 // 		}
 // 	});
 // }
+
+function howMuchILoveYou(nbPetals) {
+	let m = [
+		"I love you",
+		"a little",
+		"a lot",
+		"passionately",
+		"madly",
+		"not at all",
+	];
+	return m[(nbPetals - 1) % 6];
+}
