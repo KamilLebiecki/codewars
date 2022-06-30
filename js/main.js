6d15247f257c0279e200f942f7b94ac0c984e29a
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-06-30 06:39:19 +0200

    codewars repetition

--- js/main.js
+++ js/main.js
@@ -2258,3 +2258,8 @@
 // 		if (x[i] === "good" && ++good > 2) return "I smell a series!";
 // 	return good ? "Publish" : "Fail!";
 // }
+
+function well(x) {
+	const count = x.reduce((s, v) => s + (v == "good"), 0);
+	return count ? (count > 2 ? "I smell a series!" : "Publish") : "Fail";
+}
