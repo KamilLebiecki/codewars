eeb3c4449d8faf5755599f9356ce9cac9c11c8f6
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 14:26:10 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2475,3 +2475,8 @@
 // 	}
 // 	return countAllExes === countAllOhs;
 // }
+// function XO(str) {
+// 	let x = str.match(/x/gi);
+// 	let o = str.match(/o/gi);
+// 	return (x && x.length) === (o && o.length);
+// }
