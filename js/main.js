9392e4e4537bd22c47e137c91836d3c925272227
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 15:36:32 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2648,4 +2648,8 @@
 // 	return array[n] ** n || -1;
 // }
 
-const index = (array, n) => (array.length > n ? Math.pow(array[n], n) : -1);
+// const index = (array, n) => (array.length > n ? Math.pow(array[n], n) : -1);
+
+// function index(array, n) {
+// 	return Math.pow(array[n], n) || -1;
+// }
