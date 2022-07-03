16f2dc30bea44793b4ae62e34db0e12628a096dd
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 15:20:01 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2586,3 +2586,9 @@
 // 	}
 // 	return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
 // };
+
+// var humanYearsCatYearsDogYears = function (y) {
+// 	if (y == 1) return [1, 15, 15];
+// 	if (y == 2) return [2, 24, 24];
+// 	return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
+// };
