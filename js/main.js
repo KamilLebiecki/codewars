26aa96131894cf15924d72e89aded6a2e7877daf
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 15:40:12 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2666,3 +2666,12 @@
 // 	}
 // 	return [odd, even];
 // }
+
+// function pickIt(arr) {
+// 	let odd = [];
+// 	let even = [];
+// 	for (var x of arr) {
+// 		(x % 2 ? odd : even).push(x);
+// 	}
+// 	return [odd, even];
+// }
