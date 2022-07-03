42e726dd27ad4dd7a59c75681703ad123e87fc1a
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 10:35:26 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2361,11 +2361,11 @@
 // 	return str.split("").filter((c) => "aeiouAEIOU".includes(c)).length;
 // }
 //
-function firstNonConsecutive(arr) {
-	for (let i = 1; i < arr.length; i++) {
-		if (arr[i] - arr[i - 1] > 1) {
-			return arr[i];
-		}
-	}
-	return null;
-}
+// function firstNonConsecutive(arr) {
+// 	for (let i = 1; i < arr.length; i++) {
+// 		if (arr[i] - arr[i - 1] > 1) {
+// 			return arr[i];
+// 		}
+// 	}
+// 	return null;
+// }
