e640a4464af742897f9a8d343aa3a16e6c888640
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 14:11:28 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2425,3 +2425,8 @@
 // function howMuchILoveYou(n) {
 // 	return phrases[(n - 1) % phrases.length];
 // }
+
+// const howMuchILoveYou = (n) =>
+// 	["not at all", "I love you", "a little", "a lot", "passionately", "madly"][
+// 		n % 6
+// 	];
