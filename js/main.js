270d083ab3f001d3c544a8a07ca8a50487148f59
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 14:07:53 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2401,3 +2401,15 @@
 // 	} while (i < n);
 // 	return str;
 // }
+//
+function howMuchILoveYou(nbPetals) {
+	let m = [
+		"I love you",
+		"a little",
+		"a lot",
+		"passionately",
+		"madly",
+		"not at all",
+	];
+	return m[(nbPetals - 1) % 6];
+}
