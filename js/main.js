27f346ecbdbe220b99aba8e0a60a7e5ab67dc70f
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 15:48:32 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2701,3 +2701,13 @@
 // function disemvowel(str) {
 // 	return str.replace(/[aeiou]/gi, "");
 // }
+
+// function disemvowel(str) {
+// 	var vowels = ["a", "e", "i", "o", "u"];
+// 	return str
+// 		.split("")
+// 		.filter(function (el) {
+// 			return vowels.indexOf(el.toLowerCase()) == -1;
+// 		})
+// 		.join("");
+// }
