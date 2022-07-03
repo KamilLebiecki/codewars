cb9d5b171c7fc348dfd5e6a381414626cfa7d638
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 15:01:54 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2524,3 +2524,29 @@
 // 		})
 // 		.join(" ");
 // }
+
+// const grabDoll = (dolls) => {
+// 	let bag = [];
+// 	for (let i = 0; i < dolls.length; i++) {
+// 		if (bag.length === 3) {
+// 			break;
+// 		}
+// 		if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") continue;
+// 		bag.push(dolls[i]);
+// 	}
+// 	return bag;
+// };
+// console.log(
+// 	grabDoll([
+// 		"Hello Kitty",
+// 		"Barbie doll",
+// 		"Woody",
+// 		"Alien",
+// 		"Hello Kitty",
+// 		"Barbie doll",
+// 		"Buzz Astral",
+// 		"Sid",
+// 		"Hello Kitty",
+// 		"Barbie doll",
+// 	])
+// );
