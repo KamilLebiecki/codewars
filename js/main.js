f31e34d8b0d72004d8a805d5c48029645325dc93
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-03 10:30:06 +0200

    anki repetition

--- js/main.js
+++ js/main.js
@@ -2333,3 +2333,19 @@
 // 	} while (i < n);
 // 	return str;
 // }
+function getCount(str) {
+	let vowelsCount = 0;
+	let arr = str.split("");
+	for (let i = 0; i < arr.length; i++) {
+		if (
+			arr[i] === "a" ||
+			arr[i] === "e" ||
+			arr[i] === "i" ||
+			arr[i] === "o" ||
+			arr[i] === "u"
+		) {
+			vowelsCount++;
+		}
+	}
+	return vowelsCount;
+}
