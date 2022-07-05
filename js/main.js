201e74fe4c4c2657e336522e4b85c1f56c3facca
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-05 08:50:48 +0200

    Anki repetition

--- js/main.js
+++ js/main.js
@@ -2866,5 +2866,14 @@
 
 // const binToDec = (b) => parseInt(b.toString(), 2);
 
-const correct = (string) =>
-	string.replace(/1/g, "I").replace(/5/g, "S").replace(/0/g, "O");
+// const correct = (string) =>
+// 	string.replace(/1/g, "I").replace(/5/g, "S").replace(/0/g, "O");
+
+const correction = {
+	5: "S",
+	0: "O",
+	1: "I",
+};
+const correct = (string) => {
+	string.replace(/[501]/g, (character) => corrections[character]);
+};
