5ec797a15684b4a431214a7e7d9ab3c27521d40b
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-08 10:57:19 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3015,5 +3015,11 @@
 // 	return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
 // }
 
-const remove = (string) =>
-	string.charAt(string.length - 1) === "!" ? string.slice(0, -1) : string;
+// const remove = (string) =>
+// 	string.charAt(string.length - 1) === "!" ? string.slice(0, -1) : string;
+
+// const remove = (s) => s.replace(/!$/, "");
+
+// function remove(s) {
+// 	return s.endsWith("!") ? s.slice(0, -1) : s;
+// }
