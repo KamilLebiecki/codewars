ba6e2cade440bd00f728063dff799619c83b678e
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-10 08:47:14 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3079,3 +3079,10 @@ function find_difference(a, b) {
 			b.reduce((previous, current) => previous * current)
 	);
 }
+
+function volume(c) {
+	return c.reduce((x, y) => x * y);
+}
+function find_difference(a, b) {
+	return Math.abs(volume(a) - volume(b));
+}
