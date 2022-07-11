9fe63c3f1b9ffbdb54c316c60396d1bda0e4f9c2
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-11 08:58:00 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3123,3 +3123,20 @@
 // 	}
 // 	return bag;
 // }
+
+function getCount(str) {
+	let vowelsCount = 0;
+	let arr = str.split("");
+	for (let i = 0; i < arr.length; i++) {
+		if (
+			arr[i] === "a" ||
+			arr[i] === "e" ||
+			arr[i] === "i" ||
+			arr[i] === "o" ||
+			arr[i] === "u"
+		) {
+			vowelsCount++;
+		}
+	}
+	return vowelsCount;
+}
