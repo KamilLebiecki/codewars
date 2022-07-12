14a86c61e2aa3f569c84f9bcf1ea755587133aef
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-12 18:04:07 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3309,3 +3309,9 @@
 // 	}
 // 	return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
 // };
+
+let humanYearsCatYearsDogYears = function (y) {
+	if (y == 1) return [1, 15, 15];
+	if (y == 2) return [2, 24, 24];
+	return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
+};
