886cf6a6c341107fc31a2f5b6111b4b2d166429e
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-12 18:10:36 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3310,8 +3310,51 @@
 // 	return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
 // };
 
-let humanYearsCatYearsDogYears = function (y) {
-	if (y == 1) return [1, 15, 15];
-	if (y == 2) return [2, 24, 24];
-	return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
-};
+// let humanYearsCatYearsDogYears = function (y) {
+// 	if (y == 1) return [1, 15, 15];
+// 	if (y == 2) return [2, 24, 24];
+// 	return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
+// };
+
+// const humanYearsCatYearsDogYears = (humanYears) => {
+// 	let catYears = 0;
+// 	let dogYears = 0;
+// 	for (let i = 1; i <= humanYears; i++) {
+// 		if (i === 1) {
+// 			catYears += 15;
+// 			dogYears += 15;
+// 		} else if (i === 2) {
+// 			catYears += 9;
+// 			dogYears += 9;
+// 		} else {
+// 			catYears += 4;
+// 			dogYears += 5;
+// 		}
+// 	}
+// 	return [humanYears, catYears, dogYears];
+// };
+
+// const humanYearsCatYearsDogYears = (humanYears) => [
+// 	humanYears,
+// 	(humanYears - 1 ? 16 : 11) + 4 * humanYears,
+// 	(humanYears - 1 ? 14 : 10) + 5 * humanYears,
+// ];
+
+function disemvowel(str) {
+	return str
+		.split("")
+		.filter(
+			(e) =>
+				e !== "a" &&
+				e !== "A" &&
+				e !== "e" &&
+				e !== "E" &&
+				e !== "i" &&
+				e !== "I" &&
+				e !== "o" &&
+				e !== "O" &&
+				e !== "u" &&
+				e !== "U"
+		)
+		.join("");
+}
