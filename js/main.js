4b78c75affe0d1981ad4ab9fd4ecf3fd81bb2562
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-12 16:16:14 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3233,12 +3233,16 @@
 // 	});
 // }
 
-function pigIt(str) {
-	var arrayWord = str.split(" ");
-	return arrayWord
-		.map(function (word) {
-			var firstLetter = word.charAt(0);
-			return word.slice(1) + firstLetter + "ay";
-		})
-		.join(" ");
-}
+// function pigIt(str) {
+// 	var arrayWord = str.split(" ");
+// 	return arrayWord
+// 		.map(function (word) {
+// 			var firstLetter = word.charAt(0);
+// 			return word.slice(1) + firstLetter + "ay";
+// 		})
+// 		.join(" ");
+// }
+
+String.prototype.isUpperCase = function () {
+	return this == this.toUpperCase();
+};
