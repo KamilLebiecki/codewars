5df71f7e0904558e331e6d5c435decd01237ab6b
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-13 08:38:06 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3393,15 +3393,24 @@
 
 // const toBinary = (n) => parseInt(n.toString(2));
 
+// function pickIt(arr) {
+// 	let odd = [],
+// 		even = [];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] % 2 === 0) {
+// 			even.push(arr[i]);
+// 		} else if (arr[i] % 2 !== 0) {
+// 			odd.push(arr[i]);
+// 		}
+// 	}
+// 	return [odd, even];
+// }
+
 function pickIt(arr) {
-	let odd = [],
-		even = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] % 2 === 0) {
-			even.push(arr[i]);
-		} else if (arr[i] % 2 !== 0) {
-			odd.push(arr[i]);
-		}
+	let odd = [];
+	let even = [];
+	for (let x of arr) {
+		(x % 2 ? odd : even).push(x);
 	}
 	return [odd, even];
 }
