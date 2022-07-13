a4020678dd7aeffad7c403d4e98bba9ac51a5b06
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-13 10:50:37 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3484,6 +3484,15 @@
 
 // console.log(array.filter(isPrime));
 
-const trueOrFalse = (val) => (val ? "True" : "False");
+// const trueOrFalse = (val) => (val ? "True" : "False");
 
-const trueOrFalse = (val) => Boolean(val).toString();
+// const trueOrFalse = (val) => Boolean(val).toString();
+
+// const remove = (string) =>
+// 	string.charAt(string.length - 1) === "!" ? string.slice(0, -1) : string;
+
+// const remove = (s) => s.replace(/!$/, "");
+
+// function remove(s) {
+// 	return s.endsWith("!") ? s.slice(0, -1) : s;
+// }
