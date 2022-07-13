0d202920b46cd87d3fd7e9e617a326273c60fb41
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-13 10:59:45 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3522,12 +3522,21 @@
 
 // const stringy = (size) => Array.from(Array(size), (v, i) => +!(i % 2)).join("");
 
+// const well = (x) => {
+// 	let result = 0;
+// 	for (let i = 0; i < x.length; i++) {
+// 		if (x[i] === "good") {
+// 			result++;
+// 		}
+// 	}
+// 	return result === 0 ? "Fail" : result > 2 ? "I smell a series" : "Publish!";
+// };
+
 const well = (x) => {
-	let result = 0;
-	for (let i = 0; i < x.length; i++) {
-		if (x[i] === "good") {
-			result++;
-		}
-	}
-	return result === 0 ? "Fail" : result > 2 ? "I smell a series" : "Publish!";
+	const good_count = x.filter((x) => x === "good").length;
+	return good_count < 1
+		? "Fail"
+		: good_count < 3
+		? "Publish"
+		: "I smell a series!";
 };
