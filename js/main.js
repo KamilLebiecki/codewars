a9495aa7ae056ce487685c64e23e91b3c3c5e146
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-14 12:43:25 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3613,3 +3613,7 @@
 // 	const count = x.reduce((s, v) => s + (v === "good"), 0);
 // 	return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
 // }
+
+const sheepsArray = (arrayOfSheeps) => {
+	return arrayOfSheeps.filter(Boolean).length;
+};
