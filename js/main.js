38ecd67bfa3f0217e08eb8475fc0f252579dd30f
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-14 10:26:18 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3587,14 +3587,24 @@
 // 		: "I smell a series";
 // };
 
+// function well(x) {
+// 	switch (x.filter((i) => i === "good").length) {
+// 		case 0:
+// 			return "Fail!";
+// 		case 1:
+// 		case 2:
+// 			return "Publish!";
+// 		default:
+// 			return "I smell a series!";
+// 	}
+// }
+
 function well(x) {
-	switch (x.filter((i) => i === "good").length) {
-		case 0:
-			return "Fail!";
-		case 1:
-		case 2:
-			return "Publish!";
-		default:
+	var good = 0;
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] === "good" && ++good > 2) {
 			return "I smell a series!";
+		}
 	}
+	return good ? "Publish!" : "Fail!";
 }
