21dd13d13bec7bc588dd0bce62a4136a3a35454f
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-15 12:52:44 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3660,12 +3660,29 @@
 // const grabDoll = (dolls) =>
 // 	dolls.filter((i) => i === "Hello Kitty" || i === "Barbie doll").slice(0, 3);
 
-function grabDoll(dolls) {
-	let bag = [];
-	for (let i = 0; i < dolls.length; i++) {
-		if (!/^(Hello Kitty|Barbie doll)$/.test(dolls[i])) continue;
-		bag.push(dolls[i]);
-		if (bag.length === 3) break;
+// function grabDoll(dolls) {
+// 	let bag = [];
+// 	for (let i = 0; i < dolls.length; i++) {
+// 		if (!/^(Hello Kitty|Barbie doll)$/.test(dolls[i])) continue;
+// 		bag.push(dolls[i]);
+// 		if (bag.length === 3) break;
+// 	}
+// 	return bag;
+// }
+
+function getCount(str) {
+	let vowelsCount = 0;
+	let arr = str.split("");
+	for (let i = 0; i < arr.length; i++) {
+		if (
+			arr[i] === "a" ||
+			arr[i] === "e" ||
+			arr[i] === "i" ||
+			arr[i] === "o" ||
+			arr[i] === "u"
+		) {
+			vowelsCount++;
+		}
 	}
-	return bag;
+	return vowelsCount;
 }
