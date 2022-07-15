773001c907677374b6920a7bd0f213b60a4acf47
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-15 15:40:42 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3792,3 +3792,10 @@
 
 // const findDifference = (a, b) =>
 // 	Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2]);
+
+function find_difference(a, b) {
+	return Math.abs(
+		a.reduce((previous, current) => previous * current) -
+			b.reduce((previous, current) => previous * current)
+	);
+}
