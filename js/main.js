3d796f83dba22349998b5dfa4de677dc8cab9d1a
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-22 20:12:20 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3931,3 +3931,12 @@
 // 			return "I smell a series!";
 // 	}
 // }
+
+function firstNonConsecutive(arr) {
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] - arr[i - 1] > 1) {
+			return arr[i];
+		}
+	}
+	return null;
+}
