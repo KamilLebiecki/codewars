d6227204f4130743ad30ef1ecf400e625067ec89
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-23 20:20:34 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -3932,11 +3932,34 @@
 // 	}
 // }
 
-function firstNonConsecutive(arr) {
-	for (let i = 1; i < arr.length; i++) {
-		if (arr[i] - arr[i - 1] > 1) {
-			return arr[i];
-		}
-	}
-	return null;
-}
+// function firstNonConsecutive(arr) {
+// 	for (let i = 1; i < arr.length; i++) {
+// 		if (arr[i] - arr[i - 1] > 1) {
+// 			return arr[i];
+// 		}
+// 	}
+// 	return null;
+// }
+
+// function firstNonConsecutive(arr) {
+// 	let result = arr.find((val, index) => val !== index + arr[0]);
+// 	return Number.isInteger(result) ? result : null;
+// }
+
+// function firstNonConsecutive(arr) {
+// 	let out = null;
+// 	for (let i = 1; i < arr.length; i++) {
+// 		if (arr[i] - arr[i - 1] != 1) {
+// 			out = arr[i];
+// 			break;
+// 		}
+// 	}
+// 	return out;
+// }
+
+// const firstNonConsecutive = (arr) =>
+// 	arr.length == 1
+// 		? null
+// 		: arr[0] + 1 != arr[1]
+// 		? arr[1]
+// 		: firstNonConsecutive(arr.slice(1));
