e710aaab1979757dab346c6d2c4e85418173e77d
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-23 20:58:34 +0200

    Anki review

--- js/main.js
+++ js/main.js
@@ -4038,3 +4038,15 @@
 // }
 
 // const strCount = (str, letter) => --str.split(letter).length;
+
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+// function isPrime(num) {
+// 	fomr (let i = 2; num > i; i++) {
+// 		if (num % i == 0) {
+// 			return false;
+// 		}
+// 	}
+// 	return num > 1;
+// }
+// console.log(array.filter(isPrime));
