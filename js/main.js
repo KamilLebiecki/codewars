1b893e138ad237611c1c05fa33bc09d149918bf8
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-27 10:20:42 +0200

    Useful text

--- js/main.js
+++ js/main.js
@@ -4540,3 +4540,37 @@ const well = (x) => {
 	}
 	return result === 0 ? "Fail!" : result > 2 ? "I smell a series!" : "Publish!";
 };
+
+const well = (x) => {
+	const good_count = x.filter((x) => x === "good").length;
+	return good_count < 1
+		? "Fail"
+		: good_count < 3
+		? "Publish!"
+		: "I smell a series!";
+};
+
+function well(x) {
+	switch (x.filter((i) => i === "good").length) {
+		case 0:
+			return "Fail!";
+		case 1:
+		case 2:
+			return "Publish!";
+		default:
+			return "I smell a series!";
+	}
+}
+
+function well(x) {
+	var good = 0;
+	for (var i = 0; i < x.length; i++)
+		if (x[i] === "good" && ++good > 2) return "I smell a series!";
+
+	return good ? "Publish!" : "Fail!";
+}
+
+function well(x) {
+	const count = x.reduce((s, v) => s + (v === "good"), 0);
+	return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
+}
