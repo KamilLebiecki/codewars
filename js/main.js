b9dbe6b863d66428989d303d1f9d0ce3ab598a06
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-27 15:57:36 +0200

    removing unit tests

--- js/main.js
+++ js/main.js
@@ -4757,15 +4757,36 @@
 
 // Moving Zeros To The End
 
-function moveZeros(arr) {
-	let newArray = [];
-	let countZeros = 0;
-	for (let i = 0; i < arr.length; i++) {
-		arr[i] !== 0 && newArray.push(arr[i]);
-		arr[i] === 0 && countZeros++;
-	}
-	for (let j = 0; j < countZeros; j++) {
-		newArray.push(0);
-	}
-	return newArray;
-}
+// function moveZeros(arr) {
+// 	let newArray = [];
+// 	let countZeros = 0;
+// 	for (let i = 0; i < arr.length; i++) {
+// 		arr[i] !== 0 && newArray.push(arr[i]);
+// 		arr[i] === 0 && countZeros++;
+// 	}
+// 	for (let j = 0; j < countZeros; j++) {
+// 		newArray.push(0);
+// 	}
+// 	return newArray;
+// }
+
+// let moveZeros = (arr) =>
+// 	arr.filter((i) => i !== 0).concat(arr.filter((i) => i === 0));
+
+// let moveZeros = function (arr) {
+// 	return arr
+// 		.filter(function (x) {
+// 			return x !== 0;
+// 		})
+// 		.concat(
+// 			arr.filter(function (x) {
+// 				return x === 0;
+// 			})
+// 		);
+// };
+
+// let moveZeros = (arr) => {
+// 	let filteredList = arr.filter((num) => num !== 0);
+// 	let zeroList = arr.filter((num) => num === 0);
+// 	return filteredList.concat(zeroList);
+// };
