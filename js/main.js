eee7ca3592260788a06ff3d9d164c79e5da18c9a
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-27 10:22:50 +0200

    someone fails and it isn't me

--- js/main.js
+++ js/main.js
@@ -4531,46 +4531,59 @@
 
 // const plural = n => !== 1;
 
-const well = (x) => {
-	let result = 0;
-	for (let i = 0; i < x.length; i++) {
-		if (x[i] === "good") {
-			result++;
+// const well = (x) => {
+// 	let result = 0;
+// 	for (let i = 0; i < x.length; i++) {
+// 		if (x[i] === "good") {
+// 			result++;
+// 		}
+// 	}
+// 	return result === 0 ? "Fail!" : result > 2 ? "I smell a series!" : "Publish!";
+// };
+
+// const well = (x) => {
+// 	const good_count = x.filter((x) => x === "good").length;
+// 	return good_count < 1
+// 		? "Fail"
+// 		: good_count < 3
+// 		? "Publish!"
+// 		: "I smell a series!";
+// };
+
+// function well(x) {
+// 	switch (x.filter((i) => i === "good").length) {
+// 		case 0:
+// 			return "Fail!";
+// 		case 1:
+// 		case 2:
+// 			return "Publish!";
+// 		default:
+// 			return "I smell a series!";
+// 	}
+// }
+
+// function well(x) {
+// 	var good = 0;
+// 	for (var i = 0; i < x.length; i++)
+// 		if (x[i] === "good" && ++good > 2) return "I smell a series!";
+
+// 	return good ? "Publish!" : "Fail!";
+// }
+
+// function well(x) {
+// 	const count = x.reduce((s, v) => s + (v === "good"), 0);
+// 	return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
+// }
+
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+	for (let i = 2; num > i; i++) {
+		if (num % i === 0) {
+			return false;
 		}
 	}
-	return result === 0 ? "Fail!" : result > 2 ? "I smell a series!" : "Publish!";
-};
-
-const well = (x) => {
-	const good_count = x.filter((x) => x === "good").length;
-	return good_count < 1
-		? "Fail"
-		: good_count < 3
-		? "Publish!"
-		: "I smell a series!";
-};
-
-function well(x) {
-	switch (x.filter((i) => i === "good").length) {
-		case 0:
-			return "Fail!";
-		case 1:
-		case 2:
-			return "Publish!";
-		default:
-			return "I smell a series!";
-	}
+	return num > 1;
 }
 
-function well(x) {
-	var good = 0;
-	for (var i = 0; i < x.length; i++)
-		if (x[i] === "good" && ++good > 2) return "I smell a series!";
-
-	return good ? "Publish!" : "Fail!";
-}
-
-function well(x) {
-	const count = x.reduce((s, v) => s + (v === "good"), 0);
-	return count ? (count > 2 ? "I smell a series!" : "Publish!") : "Fail!";
-}
+console.log(array.filter(isPrime));
