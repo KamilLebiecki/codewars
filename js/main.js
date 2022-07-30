f07aee1a4496e493ac1f7ed2618a9f280f556561
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-30 20:41:40 +0200

    No time to commit.. My people need me

--- js/main.js
+++ js/main.js
@@ -5494,3 +5494,11 @@ function disemvowel(str) {
 // 		})
 // 		.join("");
 // }
+
+String.prototype.toJadenCase = function () {
+	return this.split("")
+		.map(function (w) {
+			return w.charAt(0).toUpperCase() + w.slice(1);
+		})
+		.join(" ");
+};
