35eb8e44dc6830886178c80e05b4532c42f09d6c
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2022-07-31 22:02:45 +0200

    Spinning up the hamster...

--- js/main.js
+++ js/main.js
@@ -5875,3 +5875,7 @@
 // };
 
 // Head, Tail, Init and Last
+// const head = (arr) => arr[0];
+// const tail = (arr) => arr.slice(1, arr.length);
+// const init = (arr) => arr.slice(0, arr.length - 1);
+// const last = (arr) => arr[arr.length - 1];
